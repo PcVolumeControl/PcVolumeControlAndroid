3e9de4e5b16aa101fbba9be2a5ec584bbe58b24c
Wavesonics <adamwbrown@gmail.com>
2018-05-21 22:24:16 -0700

    Upgraded to AT 1.0

--- app/src/iot/java/com/darkrockstudios/apps/pcvolumemixer/IotApp.kt
+++ app/src/iot/java/com/darkrockstudios/apps/pcvolumemixer/IotApp.kt
@@ -1,9 +1,6 @@
 package com.darkrockstudios.apps.pcvolumemixer
 
 import android.app.Application
-import android.view.Display
-import com.google.android.things.device.ScreenManager
-import java.util.concurrent.TimeUnit
 
 /**
  * Created by adamw on 12/29/2017.
@@ -13,11 +10,5 @@ class IotApp : Application()
 	override fun onCreate()
 	{
 		super.onCreate()
-
-		val screenManager = ScreenManager(Display.DEFAULT_DISPLAY)
-		// Set brightness to a fixed value
-		screenManager.setBrightnessMode(ScreenManager.BRIGHTNESS_MODE_MANUAL)
-		//screenManager.setBrightness(255) //Max it out.
-		screenManager.setScreenOffTimeout(1, TimeUnit.MINUTES)
 	}
 }
